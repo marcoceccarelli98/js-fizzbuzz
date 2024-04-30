d214ad87517500a1c9d52a4807c86072f5ec5c47
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-04-30 16:25:15 +0200

    [BONUS 1] Added DOM list

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i mult
 
 ---MAIN---
 -create a for cicle from 1 to 100
+    -check if the number is divisible by 3 and 5 and if true print FizzBuzz
     -check if the number is divisible by 3 and if true print Fizz
     -check if the number is divisible by 5 and if true print Buzz
-    -check if the number is divisible by 3 and 5 and if true print FizzBuzz
     
--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 'use strict'
 
+const ul = document.querySelector('ul.list');
+
+//create a for cicle from 1 to 100
 for (let i = 1; i <= 100; i++) {
+
+    const li = document.createElement("li");
 
     let outValue = i.toString();
 
+    //check if the number is divisible by 3 and 5 and if true print FizzBuzz
     if ((i % 3 == 0) && (i % 5 == 0)) {
         outValue = "FizzBuzz";
-    } else if (i % 3 == 0) {
+        li.classList.add('fizzbuzz');
+    } else if (i % 3 == 0) { //check if the number is divisible by 3 and if true print Fizz
         outValue = "Fizz";
-    } else if (i % 5 == 0) {
+        li.classList.add('fizz');
+    } else if (i % 5 == 0) {//check if the number is divisible by 5 and if true print Buzz
         outValue = "Buzz";
+        li.classList.add('buzz');
     }
 
-    console.log(outValue);
+    li.append(outValue);
+
+    ul.append(li);
+
 }
+
+
+
+
