bfd4f03b938fb8f0bf9b4457f1b505bbab790c62
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-04-30 14:05:13 +0200

    [UPD] Add working js script

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 'use strict'
 
+for (let i = 1; i <= 100; i++) {
+
+    let outValue = i.toString();
+
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        outValue = "FizzBuzz";
+    } else if (i % 3 == 0) {
+        outValue = "Fizz";
+    } else if (i % 5 == 0) {
+        outValue = "Buzz";
+    }
+
+    console.log(outValue);
+}
